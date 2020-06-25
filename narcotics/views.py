069c3f6d210f3c8a474378cbd3bff1d643eef334
Chris Pondoc <chris.pondoc@comcast.net>
2020-06-25 13:06:38 -0400

    Added email functionality

--- narcotics/views.py
+++ narcotics/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Narcotic
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+import smtplib, ssl, email
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.contrib.auth.models import User
+
+""" Sending Email """
+
+def email_user(receiving_email_address, supervisor, subject, drug, recorded_amount, medic_amount):
+   message = MIMEMultipart()
+   port = 465 # For SSL
+   smtp_server = "smtp.gmail.com"
+   sender_email = "frederickcountydfrs@gmail.com"
+   receiver_email = receiving_email_address
+   password = "Flava$1729"
+ 
+   SUBJECT = subject
+   FROM = sender_email
+   TO = receiver_email
+ 
+   TEXT = f"""{supervisor},
+   According to the records, the amount of {drug} has been falsely reported. The last record of {drug} is {recorded_amount}mg, however the medic is counting {medic_amount}mg. Please resolve immediately."""
+ 
+   message = "From: {}\n\nTo: {}\n\nSubject: {}\n\n{}".format(FROM, TO, SUBJECT, TEXT)
+  
+   #Create a secure SSL Context
+   context = ssl.create_default_context()
+ 
+   with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+       server.login(sender_email, password)
+       server.sendmail(sender_email, receiver_email, message)
 
 """ Narcotic Views """
 
@@ -23,7 +54,62 @@ class NarcoticListView(LoginRequiredMixin, ListView):
     # Setting the model to drug to list all the drugs
     model = Narcotic
 
-    print(type(Narcotic.objects.all()))
+    # List of all users
+    users = User.objects.all()
+
+    # List of all narcotics
+    narcotics = Narcotic.objects.all()
+
+    # Checking narcotic amounts
+    for i in range(0, len(narcotics) - 1):
+
+        # Checking if morphine in stock doesn't match
+        if (narcotics[i].morphine_in_stock != narcotics[i + 1].morphine_in_stock):
+
+            # Print alert
+            print("Sending morphine email.")
+
+            # Send email alert to admin
+            for user in users:
+                if user.groups.filter(name = "admin").exists():
+                    email_user(user.email, "chris.pondoc@gmail.com", "Morphine in Stock Alert", "Morphine", narcotics[i].morphine_in_stock, narcotics[i + 1].morphine_in_stock)
+        
+        # Checking if ketamine in stock doesn't match
+        if (narcotics[i].ketamine_in_stock_hundred_miligram_millileter != narcotics[i + 1].ketamine_in_stock_hundred_miligram_millileter):
+
+            # Print alert
+            print("Sending ketamine in stock in 100 mg/ml email.")
+
+            # Send email alert to admin
+            for user in users:
+                if user.groups.filter(name = "admin").exists():
+                    email_user(user.email, "chris.pondoc@gmail.com", "Ketamine in Stock (100 mg/ml) Alert", "Ketamine in Stock (100 mg/ml)", narcotics[i].ketamine_in_stock_hundred_miligram_millileter, narcotics[i + 1].ketamine_in_stock_hundred_miligram_millileter)
+
+        # Checking if second ketamine in stock doesn't match
+        if (narcotics[i].ketamine_in_stock_ten_miligram_millileter != narcotics[i + 1].ketamine_in_stock_ten_miligram_millileter):
+
+            # Print alert
+            print("Sending ketamine in stock in 10 mg/ml email.")
+
+            # Send email alert to admin
+            for user in users:
+                if user.groups.filter(name = "admin").exists():
+                    email_user(user.email, "chris.pondoc@gmail.com", "Ketamine in Stock (10 mg/ml) Alert", "Ketamine in Stock (10 mg/ml)", narcotics[i].ketamine_in_stock_ten_miligram_millileter, narcotics[i + 1].ketamine_in_stock_ten_miligram_millileter)
+        
+        # Checking if versed in stock doesn't match
+        if (narcotics[i].versed_in_stock != narcotics[i + 1].versed_in_stock):
+
+            # Print alert
+            print("Sending versed in stock email.")
+
+            # Send email alert to admin
+            for user in users:
+                if user.groups.filter(name = "admin").exists():
+                    email_user(user.email, "chris.pondoc@gmail.com", "versed_in_stock in Stock Alert", "versed_in_stock in Stock", narcotics[i].versed_in_stock, narcotics[i + 1].versed_in_stock)        
+        
+        # Everything is good!
+        else:
+            print("Looking good!")
 
     # Setting the template name
     template_name = 'narcotics/narcotics.html'
